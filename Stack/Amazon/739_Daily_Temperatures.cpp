c0a1b8a0b6a593854051ec9257acb453fc115412
KevinChen <threekpu@gmail.com>
2018-09-23 19:39:58 -0700

    739 Daily Temperature

---
+++ Stack/Amazon/739_Daily_Temperatures.cpp
@@ -0,0 +1,64 @@
+
+
+#include "../../commonHeader.h"
+
+//Given a list of daily temperatures, produce a list that, for each day in the input, tells you how many days you would have to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+//
+//For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+//
+//Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+
+
+class Solution {
+public:
+
+    //s1: stack
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        if(temperatures.empty())    return vector<int>();
+
+        stack<pair<int,int>> s;
+        vector<int> ans(temperatures.size(), 0);
+        for(int i = 0; i < temperatures.size(); ++i) {
+            const int t = temperatures[i];
+            if(s.empty() or s.top().first > t)    s.push(make_pair(t, i));
+            else {
+                while(not s.empty() and t > s.top().first) {
+                    ans[s.top().second] = i - s.top().second;
+                    s.pop();
+                }
+                s.push(make_pair(t,i));
+            }
+        }
+        return ans;
+    }
+
+    //s2
+    //for each day from end to start, record the next day of temperature t for all t in [30, 100]
+    //for each day from n-1 to 0; for all temperature higher than temp[i], find the earliest
+    vector<int> dailyTemperatures(vector<int>& temps) {
+        int n = temps.size();
+        vector<int> waits(n, 0);
+        vector<int> next(101, INT_MAX); // next day with with certain temperature.
+        for (int i = n - 1; i >= 0; i--) {
+            int earliest = INT_MAX;
+            for (int t = temps[i] + 1; t <= 100; t++) {
+                earliest = min(earliest, next[t]);
+            }
+            if (earliest != INT_MAX) waits[i] = earliest - i;
+            next[temps[i]] = i;
+        }
+        return waits;
+    }
+};
+
+
+int main() {
+
+    Solution Amazon;
+    vector<int> nums {1,2,3,2,1};
+    auto ans = Amazon.dailyTemperatures(nums);
+    cout << ans << endl;
+
+	return 0;
+}
+
---
+++ Tree/Google/431_Encode_N_ary_Tree_To_Binary_Tree.cpp
@@ -0,0 +1,48 @@
+
+
+#include "../../commonHeader.h"
+
+
+// Definition for a Node.
+class Node {
+public:
+    int val = NULL;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes an n-ary tree to a binary tree.
+    TreeNode* encode(Node* root) {
+        
+    }
+
+    // Decodes your binary tree to an n-ary tree.
+    Node* decode(TreeNode* root) {
+        
+    }
+};
+
+
+int main() {
+
+	return 0;
+}
+
