d3b64dbad3502f22d89868ce952d38bc3c166e33
KevinChen <threekpu@gmail.com>
2018-09-24 00:40:12 -0700

    211 Add and Search Word

---
+++ ByCompanyTag/Facebook/211_Add_and_Search_Word.cpp
@@ -0,0 +1,36 @@
+
+
+#include "../../commonHeader.h"
+
+//Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+class WordDictionary {
+public:
+    /** Initialize your data structure here. */
+    WordDictionary() {
+        
+    }
+    
+    /** Adds a word into the data structure. */
+    void addWord(string word) {
+        
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    bool search(string word) {
+        
+    }
+};
+
+
+//follow up是虽然这个已经是optimal的⽅法，有没有可能让search更快⼀点，表
+//示在insert的时候就把“.”也当做⼀个node加进去，⽐如如果insert “cat”, 那么第⼀
+//层就是 “c”,".",第⼆层是“a”,"." | "a","."
+
+
+int main() {
+    Solution Facebook;
+
+	return 0;
+}
+
