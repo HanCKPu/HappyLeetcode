dfa24eab5f2be1a35569e7083b16313980dbb1c7
KevinChen <threekpu@gmail.com>
2018-09-24 00:30:08 -0700

    76 Minimum Window Substring

---
+++ ByCompanyTag/Facebook/76_Minimum_Window_Substring.cpp
@@ -0,0 +1,48 @@
+
+
+#include "../../commonHeader.h"
+
+//Given a string S and a string T, find the minimum window in S which will contain all the characters in T in complexity O(n).
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(t.empty() or s.empty() or t.size() > s.size())   return "";
+
+        int counter = t.size();
+        unordered_map<char, int> m;
+        for(char c : t)
+            m[c]++;
+
+        pair<int,int> ans = make_pair(0, s.size()+1);
+        int l = 0, r = 0;
+        for(; r < s.size(); ++r) {
+            char c= s[r];
+            if(m.count(c) == 0)     continue;
+
+            if(m[c]-- > 0)  counter--;
+            while(counter <= 0) {
+                if(r - l < ans.second - ans.first)  ans = make_pair(l,r);   //update range
+
+                char lChar = s[l];
+                if(m.count(lChar) and m[lChar]++ >= 0)  counter++;
+                l++;
+            }
+        }
+
+        return ans.second == s.size()+1 ? "" : s.substr(ans.first, ans.second-ans.first+1);
+    }
+};
+
+
+int main() {
+    Solution Facebook;
+
+    string s = "ADOBECODEBANC";
+    string t = "ABC";
+
+    cout << Facebook.minWindow(s, t) << endl;
+
+	return 0;
+}
+
